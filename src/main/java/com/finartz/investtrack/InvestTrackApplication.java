9d63a0349def3bd4169cf4f1eb6a786723dc67cf
batuhanoz <b.ozdmr23@gmail.com>
2025-02-11 16:56:05 +0300

    Create spring boot starter project

---
+++ src/main/java/com/finartz/investtrack/InvestTrackApplication.java
@@ -0,0 +1,13 @@
+package com.finartz.investtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finartz/investtrack/InvestTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finartz.investtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
